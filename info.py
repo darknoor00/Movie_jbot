14bd2c54bdf8f76bfb4cf2a1824e8c58697badf7
darknoor00 <darknoor004@gmail.com>
2025-08-20 17:35:19 +0300

    Update info.py

--- info.py
+++ info.py
@@ -53,8 +53,8 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002330472301')  # Request cha
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+JOSoYYupAT9lMGE1')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1002330472301")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1002330472301")# Channels for force sub (make sure bot is admin)
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1002262185937")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_channels     = environ.get("AUTH_CHANNELS", "-1002635232025")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
@@ -120,9 +120,9 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+7b--w-zCC2hkZjJl') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/TheGlobalMovies') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/darknoor04') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/MovionHD') # Update channel link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/MovifyHD') # Update channel link for the bot
 
 # ============================
 # User Configuration
